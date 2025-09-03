5d0c947b42d69ca6c604fadc1e982517217753b9
WonderBattle <elenacanchog@gmail.com>
2025-09-03 12:44:51 +0200

    First commit

---
+++ src/main/java/com/Assigment2/DAT250Assigment2/Dat250Assigment2Application.java
@@ -0,0 +1,13 @@
+package com.Assigment2.DAT250Assigment2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dat250Assigment2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dat250Assigment2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/Assigment2/DAT250Assigment2/Dat250Assigment2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Assigment2.DAT250Assigment2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dat250Assigment2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
